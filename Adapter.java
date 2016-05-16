aa5d514e7005d4e7787c5d5ba6afa9526dce80b1
hui <hui8958@gmail.com>
2016-05-16 14:40:27 +0200

    formated

--- Adapter.java
+++ Adapter.java
@@ -28,42 +28,39 @@ public class Adapter extends ChangeListTestCase {
 
   int changes = 0;
 
-  public void CreateFile(){
+  public void CreateFile() {
     changes++;
     add(facade, createFile(r, "file" + nextId()));
   }
 
-  public void DeleteFile(){
-    if (!r.getChildren().isEmpty())
-    {
+  public void DeleteFile() {
+    if (!r.getChildren().isEmpty()) {
       changes++;
       add(facade, delete(r, ((Entry)r.getChildren().toArray()[0]).getPath()));
     }
   }
 
-  public void RenameFile(){
-    if (!r.getChildren().isEmpty())
-    {
+  public void RenameFile() {
+    if (!r.getChildren().isEmpty()) {
       changes++;
       add(facade, rename(r, ((Entry)r.getChildren().toArray()[0]).getPath(), "FILE" + nextId()));
     }
   }
 
-  public void CreateFolder(){
+  public void CreateFolder() {
     changes++;
-    add(facade, createDirectory(r,"dir"+nextId()));
-
+    add(facade, createDirectory(r, "dir" + nextId()));
   }
 
-  public void MoveFile(){
-    if(!r.getChildren().isEmpty()){
+  public void MoveFile() {
+    if (!r.getChildren().isEmpty()) {
       changes++;
       add(facade, move(r, ((Entry)r.getChildren().toArray()[0]).getPath(), "root2"));
     }
-
   }
-  public void ChangeContent(){
-    if(!r.getChildren().isEmpty()){
+
+  public void ChangeContent() {
+    if (!r.getChildren().isEmpty()) {
       changes++;
       add(facade, changeContent(r, ((Entry)r.getChildren().toArray()[0]).getPath(), "This is a content"));
     }
